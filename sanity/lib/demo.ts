fa44c4b2fbcd213412904eb90f9619d31cd93c52
Zahida Raees <160402259+zahidaraees@users.noreply.github.com>
2025-01-01 10:03:13 +0500

    Initial commit

---
+++ sanity/lib/demo.ts
@@ -0,0 +1,41 @@
+/**
+ * Demo data used as placeholders and initial values for the blog
+ */
+
+export const title = "Info City Hub";
+
+export const description = [
+  {
+    _key: "9f1a629887fd",
+    _type: "block",
+    children: [
+      {
+        _key: "4a58edd077880",
+        _type: "span",
+        marks: [],
+        text: "Static information about famous cities of the world ",
+      },
+      {
+        _key: "4a58edd077884",
+        _type: "span",
+        marks: [],
+        text: ".",
+      },
+    ],
+    markDefs: [
+      {
+        _key: "ec5b66c9b1e0",
+        _type: "link",
+        href: "https://nextjs.org/",
+      },
+      {
+        _key: "1f8991913ea8",
+        _type: "link",
+        href: "https://sanity.io/",
+      },
+    ],
+    style: "normal",
+  },
+];
+
+export const ogImageTitle = "A Next.js Blog with a Native Authoring Experience";
