05066c32f7cc72a59b59cf4b198d4115869bcd54
Zahida Raees <160402259+zahidaraees@users.noreply.github.com>
2025-01-12 21:18:33 +0500

    update

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,12 +1,13 @@
-import {type SchemaTypeDefination } from "sanity";
-import { Post } from "@/sanity.types";
-
+import { type SchemaTypeDefinition } from 'sanity';
 import author from "./schemas/documents/author";
 import comment from "./schemas/documents/comment";
 import post from "./schemas/documents/post";
 
-
-export const schema:{types:SchemaTypeDefination[]}={
-    types:[post, author,comment],
-
+export const Schema: { types: SchemaTypeDefinition[] } = {
+  
+  types:[post, author, comment],
 }
+
+
+
+
